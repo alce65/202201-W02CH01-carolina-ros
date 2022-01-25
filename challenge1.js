a59cd8be33c77ac65008aa88341c78b04fb7482e
Carolina Ros <rosdelreal@gmail.com>
2022-01-25 18:16:37 +0100

    Added export and import

---
+++ challenge1.js
@@ -0,0 +1,6 @@
+export const strictEquals = function (a, b) {
+    let a = 1;
+    let b = 1;
+    let result = a + b;
+    return result;
+};
---
+++ script2.js
@@ -0,0 +1,1 @@
+import { strictEquals } from "./challenge1";
