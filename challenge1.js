04f836f8ce99008f1464ec62956f5d74cc420d0e
Carolina Ros <rosdelreal@gmail.com>
2022-01-25 19:31:43 +0100

    Added conditionals

--- challenge1.js
+++ challenge1.js
@@ -1,6 +1,11 @@
 export const strictEquals = function (a, b) {
-    let a = 1;
-    let b = 1;
-    let result = a + b;
-    return result;
+    if (isNaN(a) && isNaN(b)) {
+        return false;
+    } else if (Object.is(a, 0) && Object.is(b, -0)) {
+        return true;
+    } else if (Object.is(a, -0) && Object.is(b, 0)) {
+        return true;
+    }
+
+    return Object.is(a, b);
 };
